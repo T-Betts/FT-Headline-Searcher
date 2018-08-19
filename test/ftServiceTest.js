100b902d7446e7122e8cd2fedcea0bb67aebaefc
Tom Betts <tom.betts@live.co.uk>
2018-08-19 11:59:47 +0100

    Add test that checks searchForHeadlines can return a specified page.

--- test/ftServiceTest.js
+++ test/ftServiceTest.js
@@ -4,8 +4,7 @@ const expect = require('chai').expect
 
 describe('FtService', () => {
   describe('#searchForHeadlines', () => {
-    it('should give back page 1 for a given title', (done) => {
-      nock.disableNetConnect();
+    it('should give back page 1 of results for a given query', (done) => {
       let body = {
         "queryString": "title: Example",
         "queryContext": {
@@ -22,6 +21,28 @@ describe('FtService', () => {
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } } ));
       ftService = new FtService();
       ftService.searchForHeadlines("Example", 1, (x) => {
+      expect(x.articles).to.deep.equal(['Article'])
+      done();
+      })
+    })
+
+    it('should give back page 2 of results for a given query', (done) => {
+      let body = {
+        "queryString": "title: Example",
+        "queryContext": {
+          "curations": ["ARTICLES"]
+        },
+        "resultContext": {
+          "maxResults": "20",
+          "offset": "20",
+          "contextual": true,
+          "highlight": false,
+          "aspects": ["title"]
+        }
+      }
+      let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } } ));
+      ftService = new FtService();
+      ftService.searchForHeadlines("Example", 2, (x) => {
       expect(x.articles).to.deep.equal(['Article'])
       done();
       })
