c0a0c091978aabb8be6fb8fceb31c50db6853601
Tom Betts <tom.betts@live.co.uk>
2018-08-19 13:30:26 +0100

    Refactor ftServiceTest.

--- test/ftServiceTest.js
+++ test/ftServiceTest.js
@@ -1,5 +1,6 @@
-var FtService = require('../models/ftService.js')
-const nock = require('nock')
+var FtService = require('../models/ftService.js');
+const ftService = new FtService();
+const nock = require('nock');
 const expect = require('chai').expect
 var setOffset = (offset) => {
   body = {
@@ -15,56 +16,51 @@ var setOffset = (offset) => {
       "aspects": ["title"]
     }
   }
-}
+};
 
 describe('FtService', () => {
   describe('#searchForHeadlines', () => {
     it('should give back page 1 of results for a given query', (done) => {
-      setOffset(0)
+      setOffset(0);
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } } ));
-      ftService = new FtService();
       ftService.searchForHeadlines("Example", 1, (x) => {
-        expect(x.articles).to.deep.equal(['Article'])
+        expect(x.articles).to.deep.equal(['Article']);
         done();
       })
     })
 
     it('should give back page 2 of results for a given query', (done) => {
-      setOffset(20)
+      setOffset(20);
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } } ));
-      ftService = new FtService();
       ftService.searchForHeadlines("Example", 2, (x) => {
-        expect(x.articles).to.deep.equal(['Article'])
+        expect(x.articles).to.deep.equal(['Article']);
         done();
       })
     })
 
     it('should know if the current specified page IS NOT the last page', (done) => {
-      setOffset(0)
+      setOffset(0);
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } } ));
-      ftService = new FtService();
       ftService.searchForHeadlines("Example", 1, (x) => {
-        expect(x.lastPage).to.not.be.true
+        expect(x.lastPage).to.not.be.true;
         done();
       })
     })
 
     it('should know if the current specified page IS the last page', (done) => {
-      setOffset(0)
+      setOffset(0);
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: ['Article'], indexCount: 19 } ], query: { resultContext: { maxResults: 20} } } ));
-      ftService = new FtService();
       ftService.searchForHeadlines("Example", 1, (x) => {
-        expect(x.lastPage).to.be.true
+        expect(x.lastPage).to.be.true;
         done();
       })
     })
 
     it('should return a error if a call to the FT API fails', (done) => {
-      setOffset(0)
+      setOffset(0);
       let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).replyWithError("Something went wrong")
-      ftService = new FtService();
       ftService.searchForHeadlines("Example", 1, undefined, (err) => {
-        expect(err.message).to.deep.equal('Error: Something went wrong')
+        expect(err.message).to.deep.equal('Error: Something went wrong');
         done();
       })
     })
