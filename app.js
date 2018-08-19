bc15fd1565d893bb3b05dea46162b6a2a6eab3a1
Tom Betts <tom.betts@live.co.uk>
2018-08-19 17:39:50 +0100

    Update app.js and index.ejs so that top 20 stories from FT news feed load when visiting homepage.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
+const FtService = require('./models/ftService.js');
+const ftService = new FtService();
 const PORT = process.env.PORT || 3000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => {
+    ftService.searchForHeadlines("", 1, (apiResponse) => {
+      res.render('index', apiResponse)
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- test/features/visitHomepageTest.js
+++ test/features/visitHomepageTest.js
@@ -10,6 +10,20 @@ describe('FT Headline Search homepage', () => {
   const browser = new Browser();
 
   before(function(done) {
+    let body = {
+      "queryString": "",
+      "queryContext": {
+        "curations": ["ARTICLES"]
+      },
+      "resultContext": {
+        "maxResults": "20",
+        "offset": "0",
+        "contextual": true,
+        "highlight": false,
+        "aspects": ["title"]
+      }
+    }
+    let serverMock = nock('http://api.ft.com').post('/content/search/v1', body).reply(200, JSON.stringify({ results: [ {results: [{title: {title: 'Article Title'} }], indexCount: 21 } ], query: { resultContext: { maxResults: 20} } }))
     browser.visit('/', done);
   });
 
@@ -21,6 +35,10 @@ describe('FT Headline Search homepage', () => {
     it('should have a search box', () => {
       browser.fill('search', 'Some Article');
       browser.assert.input('form input[name=search]', 'Some Article')
+    })
+
+    it('should load the FT news feed when users first visit the site', () => {
+      browser.assert.text('#link-1', 'Article Title')
     })
   })
 })
